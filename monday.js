98fda82392cc5e2e9c12fd2cf74ea7794baaa4f8
souravbhattacherjee <sourav.bhattacherjee@webspiders.com>
2025-03-24 20:11:27 +0530

    added mail

--- monday.js
+++ monday.js
@@ -814,7 +814,7 @@ export const main = async () => {
 
   // Sending the first email
   sendMail(
-    "utsab.ghosh@webspiders.com",
+    "dipesh.majumder@webspiders.com",
     prompt,
     subject,
     "sourav.bhattacherjee@webspiders.com"
@@ -822,7 +822,7 @@ export const main = async () => {
 
   // // Sending the second email
   sendMail(
-    "utsab.ghosh@webspiders.com",
+    "dipesh.majumder@webspiders.com",
     promptForDateFilter,
     subjectForDatePrompt,
     "sourav.bhattacherjee@webspiders.com"
@@ -830,7 +830,7 @@ export const main = async () => {
 
   // // Sending the third email with promptForTender
   sendMail(
-    "utsab.ghosh@webspiders.com",
+    "dipesh.majumder@webspiders.com",
     promptForTender,
     subjectForTender,
     "sourav.bhattacherjee@webspiders.com"
